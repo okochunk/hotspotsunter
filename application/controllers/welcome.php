f44bcbf8e2d399de64db1bcc4b2f70c75bf58908
hdr <hdr@jigsaw.(none)>
2013-11-28 07:44:21 +0700

    RWD login

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -18,7 +18,8 @@ class Welcome extends CI_Controller {
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
-	{
+   {
+
 		$this->load->view('welcome_message');
 	}
 }
